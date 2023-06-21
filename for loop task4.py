f414a13e5d308730f03a53b625717a4816fa74dd
“pravin09082005” <pravinmeiappane@gmail.com>
2023-06-21 20:32:37 +0530

    upated

---
+++ for loop task4.py
@@ -0,0 +1,15 @@
+z=input("enter a string you want")
+output=[]
+for i in z:
+    if(i != " "):
+        output += i
+
+        print(output)
+
+# " s u m i"
+
+# output = "" + s => s
+# output = "s" + u => su
+# output = "su" + m => sum
+# output = "sum" + i => sumi
+        
