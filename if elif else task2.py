c47063a69c1dcea42dd44195a0e28681cadd509c
“pravin09082005” <pravinmeiappane@gmail.com>
2023-06-26 00:01:56 +0530

    updated

---
+++ if elif else task2.py
@@ -0,0 +1,13 @@
+a=int(input("enter the amount of unit to calculate your bill: "))
+if(a<=100):
+    print("our bill is free")
+elif(a>=101 and a<=200 ,):
+    b=((a-100)*5)
+    print("your bill is ₹",b)
+elif(a>=201):
+    c=(500+(a-200)*10)
+    print("your bill is ₹",c)
+else:
+    print("invalid")
+
+    
