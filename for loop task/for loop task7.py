9435cf751d6c4c6fd6750aa733340633efa70c49
“pravin09082005” <pravinmeiappane@gmail.com>
2023-06-28 01:02:18 +0530

    ok..

---
+++ for loop task/for loop task7.py
@@ -0,0 +1,7 @@
+#Multiplication table....
+num = int (input("Enter the number :"))
+print ("multiplication Table of ", num)
+for i in range(1, 11):
+    print (num, "x", i,"=", num*i)
+
+    
---
+++ if tasks/if elif else task4.py
@@ -0,0 +1,7 @@
+# To find the year is a leap year..
+
+n = int (input("Enter any year"))
+if ("n % 4 = 0"):
+    print ("Leap Year")
+else:
+    print("Not a Leap Year")
