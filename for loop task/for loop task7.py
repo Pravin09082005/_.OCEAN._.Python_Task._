5dcb776903b3bd9cedf26b6407f26aa6032ea0e5
“pravin09082005” <pravinmeiappane@gmail.com>
2023-06-29 13:11:24 +0530

    updated

--- for loop task/for loop task7.py
+++ for loop task/for loop task7.py
@@ -1,7 +1,8 @@
 #Multiplication table....
 num = int (input("Enter the number :"))
+z=int(input("enter the range of the table:"))
 print ("multiplication Table of ", num)
-for i in range(1, 11):
+for i in range(1, z+1):
     print (num, "x", i,"=", num*i)
 
     
