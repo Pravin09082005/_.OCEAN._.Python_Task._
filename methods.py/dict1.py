3e4c602e3c1a61550f3ac0dc160091fd9d80e8a7
“pravin09082005” <pravinmeiappane@gmail.com>
2023-07-06 19:24:49 +0530

    baka..

---
+++ methods.py/dict1.py
@@ -0,0 +1,13 @@
+def factorial():
+    num = int(input("Enter a number: "))
+    z = 1   #factoral
+    x = 1   #counter value
+    while x <= num:
+        z *= x
+        x += 1
+    print("The factorial of", num, "is", z)
+
+
+factorial()
+
+
