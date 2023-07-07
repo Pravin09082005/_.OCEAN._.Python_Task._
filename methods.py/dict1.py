d32f8f9718281c2d89a6efd02ef016f9315d4aa4
“pravin09082005” <pravinmeiappane@gmail.com>
2023-07-07 17:25:49 +0530

    newbi...

--- methods.py/dict1.py
+++ methods.py/dict1.py
@@ -10,4 +10,22 @@ def factorial():
 
 factorial()
 
+def name_in_box():
+    user = input("enter your name: ")
+    l = len(user)
 
+# for i in range(l):
+#     print(f" -----------\n|           |\n|     {user[i]}     |\n|           |\n -----------")
+
+    for i in range(1, 6):
+        if(i == 1 or i == 5):
+            print(" -----------     " * l)
+        elif(i == 2 or i == 4):
+            print("|           |    " * l)
+        elif(i==3):
+            for j in range(l):
+                print(f"|     {user[j]}     |    " ,end="")
+            print()
+
+
+name_in_box()
