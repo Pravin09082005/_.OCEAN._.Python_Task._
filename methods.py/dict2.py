d9e0a0f028a930457fd02666d76893e90bf4a6c2
“pravin09082005” <pravinmeiappane@gmail.com>
2023-07-07 21:55:17 +0530

    newbi...

--- methods.py/dict2.py
+++ methods.py/dict2.py
@@ -1,4 +1,4 @@
-def reversing(s):
+def reversing():
     s=str(input("Enter a string to be reversed:"))
     length=len(s)
     z=" "
@@ -9,3 +9,24 @@ def reversing(s):
     
 reversing()
 
+
+
+#--------------------------------------------------------------------------------------
+print()
+#-having input being fed(dynamic)
+#-having input fed already(static)
+#--------------------------------------------------------------------------------------
+
+
+def reversing(s):
+    length = len(s)
+    z = ""
+    while length > 0:
+        length -= 1
+        z += s[length]
+    return z
+
+input = "pravin"  
+
+reversed = reversing(input)
+print(reversed)
