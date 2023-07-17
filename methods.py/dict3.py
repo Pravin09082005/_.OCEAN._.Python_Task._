d620c8ee9f40508a9c03d973dae2dd6bdb78d73a
“pravin09082005” <pravinmeiappane@gmail.com>
2023-07-17 20:01:02 +0530

    updated

--- methods.py/dict3.py
+++ methods.py/dict3.py
@@ -7,3 +7,16 @@ def product(z):
 
 
 print(product([12,2,1,0]))
+
+
+def multiply(z):
+    p = 1
+    for i in z:
+        # if i!=0:
+            p *= i
+    return p
+
+
+print(multiply([12,2,1,0]))
+
+
