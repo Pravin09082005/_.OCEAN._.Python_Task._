79613e382e3f5c010d261db001904915c6cb336e
“pravin09082005” <pravinmeiappane@gmail.com>
2025-01-15 13:38:04 +0530

    updated and modified

--- sem2.py
+++ sem2.py
@@ -229,4 +229,13 @@ while n <= num:
 print("The factorial of", num, "is", z)
 
 """
-
+"""
+def fact(n):
+    if n==1:
+        return(1)
+    else :
+        return(n*fact(n-1))
+    
+n=int(input("Enter a number:"))
+print("Factorial value:",fact(n))
+"""
